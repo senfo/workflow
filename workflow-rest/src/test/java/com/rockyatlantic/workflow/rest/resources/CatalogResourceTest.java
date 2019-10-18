95faacd5d27a8c3eebf0a8b6d3904d58e0bab3a0
Fao, Sean <sean.fao@gmail.com>
2019-10-18 12:45:55 -0600

    Added simple unit test.

---
+++ workflow-rest/src/test/java/com/rockyatlantic/workflow/rest/resources/CatalogResourceTest.java
@@ -0,0 +1,28 @@
+package com.rockyatlantic.workflow.rest.resources;
+
+import com.rockyatlantic.workflow.models.Catalog;
+import com.rockyatlantic.workflow.rest.service.CatalogService;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+import java.util.UUID;
+
+/**
+ * Ensures the {@link CatalogResource} class is functioning as expected
+ */
+public class CatalogResourceTest {
+    @Test
+    public void post() {
+        CatalogResource catalogResource = new CatalogResource(new CatalogService());
+        Catalog catalog = new Catalog();
+
+        // TODO: We'll add more later
+        catalog.setCataLogId(UUID.randomUUID());
+
+        Response response = catalogResource.post(catalog);
+        Response.StatusType statusType = response.getStatusInfo();
+
+        assert statusType.getStatusCode() == 200 || statusType.getStatusCode() == 201;
+        assert statusType.getReasonPhrase().equals("Created");
+    }
+}
